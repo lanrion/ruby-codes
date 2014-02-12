5b4a47fcaee91cf78a37a3eab4353d4168561337
lanrion <huaitao-deng@foxmail.com>
2014-02-12 14:09:10 +0800

    texst extend

---
+++ extend_method.rb
@@ -0,0 +1,28 @@
+module ExtendMethod
+  module ClassMethods
+    def pp
+      puts Time.now.to_s + "  ClassMethods"
+    end
+  end
+
+  module InstanceMethods
+    def pp
+      puts Time.now.to_s + "  InstanceMethods"
+    end
+  end
+
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+end
+
+class User
+  include ExtendMethod
+end
+
+User.new.pp
+
+User.pp
+
+Time.now.utc.strftime("%Y%m%d%H%M%S")
